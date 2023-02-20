e744d142f789c59ee8fd3cc5d3c800ac62c49a01
IlyhaW <125214907+IlyhaW@users.noreply.github.com>
2023-02-20 22:14:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,8 +15,8 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
     uint64_t number = 0;
     uint64_t result = 0;
-    while(n > 0) {
-        if(checkPrime(number) == 1) {
+    while (n > 0) {
+        if (checkPrime(number) == 1) {
             result = number;
             --n;
         }
@@ -35,13 +35,13 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-   uint64_t sum = 0;
-   hbound -= 1;
-   while(hbound>=1) {
-       if(checkPrime(hbound) == true) {
-           sum += hbound;
-       }
-       --hbound;
-   }
-   return sum;
+    uint64_t sum = 0;
+    hbound -= 1;
+    while (hbound >= 1) {
+        if (checkPrime(hbound) == true) {
+            sum += hbound;
+        }
+        --hbound;
+    }
+    return sum;
 }
